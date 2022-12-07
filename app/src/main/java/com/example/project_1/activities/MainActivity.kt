81cf90c10c80f604fc3b4e718dd5b91b060768a6
emrekuruu <emre.kuru@ozu.edu.tr>
2022-12-07 12:34:47 +0300

    Updates

--- app/src/main/java/com/example/project_1/activities/MainActivity.kt
+++ app/src/main/java/com/example/project_1/activities/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.project_1.Activities
 
+import PlayerFragment
+import TeamFragment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import androidx.fragment.app.FragmentContainer
 import com.example.project_1.R
-import com.example.project_1.adapter.TeamAdapter
-import com.example.project_1.data.PlayerDataSource
-import com.example.project_1.data.TeamDataSource
+import com.example.project_1.fragments.HomeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +16,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val playerFragment = PlayerFragment()
+        val teamFragment = TeamFragment()
+        val homeFragment = HomeFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainerView,homeFragment)
+            commit()
+        }
+
         val menu : BottomNavigationView = findViewById(R.id.bottomNav)
 
         menu.setOnNavigationItemSelectedListener {
             when(it.itemId){
-                R.id.teamslogo -> Intent(this,TeamsActivity::class.java).also{startActivity(it)}
-                R.id.playerlogo -> Intent(this,PlayersActivity::class.java).also{startActivity(it)}
-                else -> {}
+                R.id.HomePage ->
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainerView,homeFragment)
+                        commit()
+                    }
+                R.id.teamslogo ->      supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.fragmentContainerView,teamFragment)
+                    commit()
+                }
+                R.id.playerlogo ->      supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.fragmentContainerView,playerFragment)
+                    commit()
+                }
             }
             true
         }
